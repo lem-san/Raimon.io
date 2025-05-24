d153aa5f9bb2cc9fd4dab1dcca738b266fea9e9f
Raimon <raimon.barragans@gmail.com>
2025-05-24 23:37:54 +0900

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import logo from '../assets/logo.svg'
+import './Nav.css'
+
+export default function Nav() {
+
+    const [colorMode, setColorMode] = useState("light")
+
+    function handleHome() {
+        return (
+            alert('THIS WORKS')
+        )
+    }
+
+    function handleMode() {
+        setColorMode(prevMode => prevMode === "light" ? "dark" : "dark")
+        document.body.classList.toggle("dark")
+    }
+
+    return (
+        <div className='navBar'>
+            <img className='logo' src={logo} alt='logo' onClick={handleHome}/>
+            <div className='darkModeToggle'>
+                <input type="checkbox" class="checkbox" id="checkbox" onChange={handleMode} />
+                <label for="checkbox" class="checkbox-label">
+                    <i class="fas fa-moon"></i>
+                    <i class="fas fa-sun"></i>
+                    <span class="ball"></span>
+                </label>
+            </div>
+        </div>           
+)
+}
